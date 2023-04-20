1526cb8f5d6b1af4b7dec50e490e7c5842f5733a
신 우현 <woo_hyun_1@naver.com>
2023-04-20 20:36:15 +0900

    Learn about redirect/rewrite, getServerSideProps, Two method of Navigation(Link/router.push)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,3 +26,36 @@ export default function Home() {
     </div>
   );
 }
+
+// getServerSideProps(완전한 SSR) ⭐️
+
+// page에서 서버 측 랜더링 함수인 getServerSideProps함수를 export하는 경우
+// Next.js는 getServerSideProps에서 반환된 데이터를 사용하여 각 request에서 이 페이지를 pre-render합니다.
+// getServerSideProps는 서버 측에서만 실행되며 브라우저에서는 실행되지 않습니다.(SEO가 필요한 페이지에서 SSR을 적용할 수 있다.)
+
+// return한 props는 각 페이지(컴포넌트)의 props로 받는다.(_app.tsx의 pageProps로 전달)
+// ex) fetch, DB 요청, API 불러오기, API key 사용 등
+
+// https://nextjs.org/docs/basic-features/data-fetching/get-server-side-props
+
+////////////////////////////////////////
+
+// Next.js 에서 Navigate하는 방법 2가지 ☑️
+
+// 1. <Link href="URL"></Link>
+// 2. useRouter Hook을 사용하여 이벤트 핸들러에 router.push(URL) 추가
+// ex) form 제출 시 페이지 이동
+
+// 'router.push의 인자', '<Link>의 href props'로 url외에 query 속성을 통해 다른 정보들을 전달할 수 있고,
+// 두번째 인자인 as를 통해 "마스킹(Masking)"하여 그 정보를 노출시키지 않을 수 있다.
+
+// router.push({
+//   pathname: '/post/[pid]',
+//   query: { pid: post.id },
+//   })
+
+export async function getServerSideProps() {
+  return {
+    props: {},
+  };
+}
