6702380e087bb8ce0cccd83d3964f7d82f1c46de
신 우현 <woo_hyun_1@naver.com>
2023-05-08 21:19:09 +0900

    [feat] Create contest page and render contest list

--- src/interfaces/contestInterface.ts
+++ src/interfaces/contestInterface.ts
@@ -2,6 +2,39 @@ export interface IContestParams {
   token: string | null;
   keyword: string;
   filterBy: string;
+  orderBy: string;
   page: number;
   size: number;
+}
+
+export interface IHost {
+  uid: number;
+  name: string;
+}
+
+export interface IContestInfo {
+  argeements: string[];
+  category: string;
+  competitionId: number;
+  competitionType: string;
+  content: string;
+  createdDate: string;
+  endDate: string;
+  host: IHost;
+  location: string;
+  locationDetail: string;
+  maxPlayer: number | null;
+  maxViewer: number | null;
+  name: string;
+  posters: string[];
+  recruitingEnd: string;
+  recruitingStart: string;
+  scrapCount: string;
+  startDate: string;
+  state: string;
+  stateDetail: string | null;
+  templateID: string;
+  totalPrize: number;
+  updatedDate: string;
+  viewCount: number;
 }
