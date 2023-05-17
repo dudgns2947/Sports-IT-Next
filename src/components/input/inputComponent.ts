29737096d2c1f11664885ad7d1801dd1366ae0bb
신 우현 <woo_hyun_1@naver.com>
2023-05-18 03:58:58 +0900

    [design] Create rules-and-terms page

---
+++ src/components/input/inputComponent.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Input = styled.input`
+  width: 335px;
+  height: 45px;
+  background: #f9f9fa;
+  border: 1px solid #ededed;
+  border-radius: 8px;
+  font-weight: 400;
+  font-size: 14px;
+  color: #aeaeae;
+  padding: 14px 20px;
+`;
+
+export const ShortInput = styled(Input)`
+  width: 255px;
+`;
