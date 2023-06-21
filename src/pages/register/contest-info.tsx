6337dff96e1509b645ffe995a770ae2c2244f2d3
신 우현 <woo_hyun_1@naver.com>
2023-06-21 22:08:41 +0900

    [fix] Bug fix of totalPrize null error

--- src/pages/register/contest-info.tsx
+++ src/pages/register/contest-info.tsx
@@ -3,11 +3,18 @@ import { PageWrapper } from "@component/components/container/container";
 import GoBackHeader from "@component/components/header/GoBackHeader";
 import React, { use, useEffect, useState } from "react";
 import { useRecoilState, useRecoilValue } from "recoil";
-import { Input, InputArea, InputTitle } from "../../styles/register/headcount.styles";
+import {
+  Input,
+  InputArea,
+  InputTitle,
+} from "../../styles/register/headcount.styles";
 import NavBar from "@component/components/navbar/NavBar";
 import * as S from "../../styles/register/contest-info.styles";
 import Link from "next/link";
-import { ContentArea, ContentPaddingArea } from "@component/components/area/areaComponent";
+import {
+  ContentArea,
+  ContentPaddingArea,
+} from "@component/components/area/areaComponent";
 import {
   contestEndDateAtom,
   contestLocationAtom,
@@ -29,13 +36,21 @@ const ContestInfo = () => {
   const [contestName, setContestName] = useRecoilState(contestNameAtom);
   const [startDate, setStartDate] = useRecoilState(contestStartDateAtom);
   const [endDate, setEndDate] = useRecoilState(contestEndDateAtom);
-  const [recruitingStart, setRecruitingStart] = useRecoilState(contestRecruitingStartAtom);
-  const [recruitingEnd, setRecruitingEnd] = useRecoilState(contestRecruitingEndAtom);
+  const [recruitingStart, setRecruitingStart] = useRecoilState(
+    contestRecruitingStartAtom
+  );
+  const [recruitingEnd, setRecruitingEnd] = useRecoilState(
+    contestRecruitingEndAtom
+  );
   const [totalPrize, setTotalPrize] = useRecoilState(contestTotalPrizeAtom);
   const [location, setLocation] = useRecoilState(contestLocationAtom) || "";
-  const [locationDetail, setLocationDetail] = useRecoilState(contestLocationDetailAtom);
-  const [CurrentLongitute, setCurrentLongitute] = useRecoilState(contestLatitudeAtom) || "";
-  const [CurrentLatitude, setCurrentLatitude] = useRecoilState(contestLongitudeAtom) || "";
+  const [locationDetail, setLocationDetail] = useRecoilState(
+    contestLocationDetailAtom
+  );
+  const [CurrentLongitute, setCurrentLongitute] =
+    useRecoilState(contestLatitudeAtom) || "";
+  const [CurrentLatitude, setCurrentLatitude] =
+    useRecoilState(contestLongitudeAtom) || "";
 
   const router = useRouter();
 
@@ -94,24 +109,56 @@ const ContestInfo = () => {
           <InputArea>
             <InputTitle>대회 일정</InputTitle>
             <S.SmallInputWrapper>
-              <S.SmallInput onChange={(e) => setStartDate(new Date(e.currentTarget.value).toISOString())} type="date" />
+              <S.SmallInput
+                onChange={(e) =>
+                  setStartDate(new Date(e.currentTarget.value).toISOString())
+                }
+                type="date"
+              />
               <S.Text>부터</S.Text>
-              <S.SmallInput onChange={(e) => setEndDate(new Date(e.currentTarget.value).toISOString())} type="date" />
+              <S.SmallInput
+                onChange={(e) =>
+                  setEndDate(new Date(e.currentTarget.value).toISOString())
+                }
+                type="date"
+              />
               <S.Text>까지</S.Text>
             </S.SmallInputWrapper>
           </InputArea>
           <InputArea>
             <InputTitle>모집 기간</InputTitle>
             <S.SmallInputWrapper>
-              <S.SmallInput onChange={(e) => setRecruitingStart(new Date(e.currentTarget.value).toISOString())} type="date" />
+              <S.SmallInput
+                onChange={(e) =>
+                  setRecruitingStart(
+                    new Date(e.currentTarget.value).toISOString()
+                  )
+                }
+                type="date"
+              />
               <S.Text>부터</S.Text>
-              <S.SmallInput onChange={(e) => setRecruitingEnd(new Date(e.currentTarget.value).toISOString())} type="date" />
+              <S.SmallInput
+                onChange={(e) =>
+                  setRecruitingEnd(
+                    new Date(e.currentTarget.value).toISOString()
+                  )
+                }
+                type="date"
+              />
               <S.Text>까지</S.Text>
             </S.SmallInputWrapper>
           </InputArea>
           <InputArea>
             <InputTitle>총 상금</InputTitle>
-            <S.LargeInput type="number" placeholder="ex) 10,000,000" onChange={(e) => setTotalPrize(parseInt(e.currentTarget.value))} />
+            <S.LargeInput
+              type="number"
+              placeholder="ex) 10,000,000"
+              onChange={(e) =>
+                e.currentTarget.value !== ""
+                  ? setTotalPrize(parseInt(e.currentTarget.value))
+                  : 0
+              }
+            />
           </InputArea>
           <InputArea>
             <InputTitle>개최 장소</InputTitle>
@@ -123,10 +170,18 @@ const ContestInfo = () => {
               }}
               value={location!}
             />
-            <Input type="text" placeholder="상세 주소 입력" onChange={(e) => setLocationDetail(e.currentTarget.value)} />
+            <Input
+              type="text"
+              placeholder="상세 주소 입력"
+              onChange={(e) => setLocationDetail(e.currentTarget.value)}
+            />
           </InputArea>
           {location ? (
-            <Map latitude={parseFloat(CurrentLatitude!)} longitude={parseFloat(CurrentLongitute!)} addressName={location!} />
+            <Map
+              latitude={parseFloat(CurrentLatitude!)}
+              longitude={parseFloat(CurrentLongitute!)}
+              addressName={location!}
+            />
           ) : null}
           {/* </S.InputWrapper> */}
         </ContentPaddingArea>
